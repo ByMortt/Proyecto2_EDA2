15b942d927c59a18b5543224a5117f8ecbd222c6
Oscar Cruz <121973352+ByMortt@users.noreply.github.com>
2023-10-08 15:45:42 -0600

    V3.2 proyecto documentacion incluida

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Alumno"},{"p":"<Unnamed>","l":"Asignatura"},{"p":"<Unnamed>","l":"BusquedaBinaria"},{"p":"<Unnamed>","l":"BusquedaLineal"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
